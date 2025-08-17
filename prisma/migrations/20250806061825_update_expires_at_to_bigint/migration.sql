8510e4ad2d2090c424fc302c91935cd81b7d858e
akhbarss <akbar25005@gmail.com>
2025-08-17 18:10:41 +0700

    delete: migration

--- prisma/migrations/20250806061825_update_expires_at_to_bigint/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `token` MODIFY `expiresAt` BIGINT NOT NULL;
--- prisma/migrations/20250814173306_add_cascade_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `token` DROP FOREIGN KEY `Token_shopId_fkey`;
-
--- AddForeignKey
-ALTER TABLE `Token` ADD CONSTRAINT `Token_shopId_fkey` FOREIGN KEY (`shopId`) REFERENCES `Shop`(`shopId`) ON DELETE CASCADE ON UPDATE CASCADE;
